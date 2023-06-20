10c674f18b9537525cc2653834cdb12322fabcf8
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-06-20 10:16:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const lightning = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     lightning.classList.add('lightning');
 
-    // SVG-Code für den Blitz
-    const svgCode = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="40" viewBox="0 0 20 40">
-        <polygon points="10,0 18,30 2,30" />
-      </svg>
-    `;
-
-    lightning.innerHTML = svgCode;
-    lightningContainer.appendChild(lightning);
+    // Lade die externe SVG-Datei
+    fetch('crack.svg')
+      .then(response => response.text())
+      .then(svgData => {
+        lightning.innerHTML = svgData;
+        lightningContainer.appendChild(lightning);
+      });
   }
 
   function generateLightnings() {
