6151b3f011d5375b0a2bc6c74a910459d8db73fc
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-06-20 09:05:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-  const neonFlash = document.getElementById('neonFlash');
+  const lightningContainer = document.getElementById('lightning-container');
+  const numParticles = 100;
 
-  function flashAnimation() {
-    neonFlash.style.opacity = 0.8;
-    setTimeout(() => {
-      neonFlash.style.opacity = 0.1;
-    }, 1000);
+  function createLightning() {
+    const lightning = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    lightning.classList.add('lightning');
+    lightning.setAttribute('viewBox', '0 0 100 100');
+
+    const polygon = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+    polygon.setAttribute('points', '10,0 90,0 50,100');
+
+    lightning.appendChild(polygon);
+    lightningContainer.appendChild(lightning);
   }
 
-  setInterval(flashAnimation, 4000);
+  function generateLightnings() {
+    for (let i = 0; i < numParticles; i++) {
+      createLightning();
+    }
+  }
+
+  generateLightnings();
 });
