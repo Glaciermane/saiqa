da2c7f799bbcaa896271263d7661ba064bf68066
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-06-21 08:03:15 +0200

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -1,25 +1,26 @@
-function updateVisibility() {
-  var triangles = document.querySelectorAll(".triangle");
-  for (var i = 0; i < triangles.length; i++) {
-    var triangle = triangles[i];
-    var isVisible = Math.random() < 0.5; // Zufällige Entscheidung, ob das Dreieck ein- oder ausgeblendet wird
+function generateTriangles() {
+  var svg = document.querySelector("svg");
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var numberOfTriangles = 500;
 
-    if (isVisible) {
-      triangle.style.animation = "fadeIn 2s ease-in-out"; // Animation für Einblendung
-      triangle.style.opacity = 1;
-    } else {
-      triangle.style.animation = "fadeOut 2s ease-in-out"; // Animation für Ausblendung
-    }
+  svg.innerHTML = "";
 
-    var x1 = Math.random() * window.innerWidth;
-    var y1 = Math.random() * window.innerHeight;
+  for (var i = 0; i < numberOfTriangles; i++) {
+    var triangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    var x1 = Math.random() * width;
+    var y1 = Math.random() * height;
     var x2 = x1 + Math.random() * 2 - 1;
     var y2 = y1 + Math.random() * 2 - 1;
     var x3 = x1 + Math.random() * 2 - 1;
     var y3 = y1 + Math.random() * 2 - 1;
     triangle.setAttribute("points", x1 + "," + y1 + " " + x2 + "," + y2 + " " + x3 + "," + y3);
+    triangle.classList.add("triangle");
+    svg.appendChild(triangle);
   }
 }
+
+// Restlicher Code...
 
 document.addEventListener("DOMContentLoaded", function() {
   generateTriangles(); // Dreiecke initial generieren
