51fe4ba05dd38b32f38fe71fd20b4c91e6d59adc
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-06-21 06:44:37 +0200

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
   var svg = document.querySelector("svg");
   var width = window.innerWidth;
   var height = window.innerHeight;
-  var numberOfTriangles = 300; // Anzahl der Dreiecke, die generiert werden sollen
+  var numberOfTriangles = 200; // Anzahl der Dreiecke, die generiert werden sollen
 
   for (var i = 0; i < numberOfTriangles; i++) {
     var triangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-    var x1 = Math.random() * width;
-    var y1 = Math.random() * (height + 100) + height; // Startposition außerhalb des sichtbaren Bereichs (unten)
+    var x1 = Math.random() * (width - 50) + 25; // x-Koordinate in der Mitte des Bildschirms
+    var y1 = Math.random() * (height - 50) + 25; // y-Koordinate in der Mitte des Bildschirms
     var x2 = x1 + Math.random() * 20 - 10;
     var y2 = y1 + Math.random() * 20 - 10;
     var x3 = x1 + Math.random() * 20 - 10;
