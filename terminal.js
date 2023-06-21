2fbd689f162ebc087ae5096ea97f2fce14957a52
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-06-21 10:20:31 +0200

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -2,9 +2,20 @@ function generateTriangles() {
   var svg = document.querySelector("svg");
   var width = window.innerWidth;
   var height = window.innerHeight;
-  var numberOfTriangles = 500;
+  var triangles = document.querySelectorAll(".triangle");
 
-  svg.innerHTML = "";
+  triangles.forEach(function(triangle) {
+    var isVisible = parseFloat(triangle.style.opacity) > 0;
+    if (isVisible) {
+      triangle.style.opacity = "0";
+      triangle.style.transition = "opacity 1s ease-in-out";
+      triangle.addEventListener("transitionend", function() {
+        triangle.parentNode.removeChild(triangle);
+      });
+    }
+  });
+
+  var numberOfTriangles = 1000 - triangles.length;
 
   for (var i = 0; i < numberOfTriangles; i++) {
     var triangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
@@ -20,26 +31,39 @@ function generateTriangles() {
   }
 }
 
-function animateTriangle(triangle) {
-  if (triangle.style.opacity === "0") {
-    triangle.style.opacity = "1";
-    triangle.style.transition = "opacity 1s ease-in-out";
-    triangle.style.pointerEvents = "auto";
-  } else {
-    triangle.style.opacity = "0";
-    triangle.style.transition = "opacity 1s ease-in-out";
-    triangle.style.pointerEvents = "none";
-  }
+function updateVisibility() {
+  var triangles = document.querySelectorAll(".triangle");
+  triangles.forEach(function(triangle) {
+    var isVisible = Math.random() < 0.5;
+    if (isVisible) {
+      triangle.style.opacity = "1";
+      triangle.style.transition = "opacity 2s ease-in-out";
+    } else {
+      triangle.style.opacity = "0";
+      triangle.style.transition = "opacity 2s ease-in-out";
+    }
+  });
 }
 
-function animateRandomTriangles() {
+function updatePositions() {
   var triangles = document.querySelectorAll(".triangle");
-  var randomIndex = Math.floor(Math.random() * triangles.length);
-  var randomTriangle = triangles[randomIndex];
-  animateTriangle(randomTriangle);
+  triangles.forEach(function(triangle) {
+    var isVisible = parseFloat(triangle.style.opacity) > 0;
+    if (isVisible) {
+      var x1 = Math.random() * window.innerWidth;
+      var y1 = Math.random() * window.innerHeight;
+      var x2 = x1 + Math.random() * 5 - 2.5;
+      var y2 = y1 + Math.random() * 5 - 2.5;
+      var x3 = x1 + Math.random() * 5 - 2.5;
+      var y3 = y1 + Math.random() * 5 - 2.5;
+      triangle.setAttribute("points", x1 + "," + y1 + " " + x2 + "," + y2 + " " + x3 + "," + y3);
+    }
+  });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  generateTriangles();
-  setInterval(animateRandomTriangles, 2000);
+  generateTriangles(); // Dreiecke initial generieren
+  setInterval(updateVisibility, 2000); // Einblendung, Ausblendung aktualisieren (alle 2 Sekunden)
+  setInterval(updatePositions, 10000); // Positionswechsel aktualisieren (alle 5 Sekunden)
+  setInterval(generateTriangles, 10000); // Dreiecke regelmäßig neu generieren (alle 10 Sekunden)
 });
