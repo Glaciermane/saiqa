35b132d731cd2aa12ca1db3afdf1823ccfaae53a
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-06-21 13:09:36 +0200

    Update cmd.js

--- cmd.js
+++ cmd.js
@@ -1,7 +1,59 @@
-const inputField = document.querySelector('.input-field');
-const outputDiv = document.querySelector('.output');
+function displayOutput(output) {
+  const outputElement = document.getElementById('output');
+  const timestamp = getTimestamp();
 
-// Funktion, um Befehle zu verarbeiten und die entsprechende Antwort zu generieren
+  const outputItem = document.createElement('div');
+  outputItem.className = 'output-item';
+
+  const timestampSpan = document.createElement('span');
+  timestampSpan.className = 'timestamp';
+  timestampSpan.textContent = timestamp;
+  outputItem.appendChild(timestampSpan);
+
+  let currentIndex = 0;
+  const typingSpeed = 40;
+
+  const typingInterval = setInterval(() => {
+    if (currentIndex === output.length) {
+      clearInterval(typingInterval);
+    } else if (output[currentIndex] === '<') {
+      const endIndex = output.indexOf('>', currentIndex) + 1;
+      const tag = output.substring(currentIndex, endIndex);
+
+      const tagSpan = document.createElement('span');
+      tagSpan.innerHTML = tag;
+
+      outputItem.appendChild(tagSpan);
+      currentIndex = endIndex;
+    } else {
+      const char = output[currentIndex];
+      const span = document.createElement('span');
+      span.textContent = char;
+
+      if (output.substring(currentIndex, currentIndex + 9) === 'unknown cmd') {
+        span.classList.add('yellow');
+        currentIndex += 8;
+      }
+
+      outputItem.appendChild(span);
+      currentIndex++;
+    }
+  }, typingSpeed);
+
+  outputElement.appendChild(outputItem);
+}
+
+
+
+
+function getTimestamp() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+  return `[${hours}:${minutes}:${seconds}]`;
+}
+// Funktion zum Verarbeiten des eingegebenen Befehls
 function processCommand(command) {
   let output = '';
 
@@ -13,18 +65,23 @@ function processCommand(command) {
   } else if (command === 'info') {
     output = 'Dies ist ein Beispiel-Terminal.';
   } else {
-    output = 'Befehl nicht erkannt: ' + command;
+    output = ' ' + '│' + ' ' + ' ' + 'unkown cmd:' + '  ' + command;
   }
 
-  // Antwort im Terminal anzeigen
-  outputDiv.textContent = output;
+  displayOutput(output);
 }
 
-// Event Listener für das Eingabefeld, um den Befehl zu verarbeiten
-inputField.addEventListener('keydown', function(event) {
-  if (event.key === 'Enter') {
-    const command = inputField.value.trim();
-    processCommand(command);
-    inputField.value = ''; // Eingabefeld leeren
-  }
+document.addEventListener('DOMContentLoaded', function() {
+  var inputField = document.getElementById('input-field');
+
+  inputField.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      var command = inputField.value.trim();
+      processCommand(command);
+      inputField.value = ''; // Eingabefeld leeren
+    }
+  });
 });
+
+
+
